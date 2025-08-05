e7dbe6b04c804f9d31d197eacdb2738ab2073dbc
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2025-08-04 23:58:12 -0300

    Add initial tests/features.

--- src/forkbridge/core.clj
+++ src/forkbridge/core.clj
@@ -1,6 +1,25 @@
-(ns forkbridge.core)
+(ns forkbridge.core
+  (:import [java.lang ProcessBuilder]
+           [java.io BufferedReader InputStreamReader OutputStreamWriter]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn start-process
+  [cmd-vec]
+  (let [pb (ProcessBuilder. cmd-vec)
+        proc (.start pb)
+        stdin-writer  (-> proc .getOutputStream OutputStreamWriter.)
+        stdout-reader (-> proc .getInputStream InputStreamReader. BufferedReader.)
+        stderr-reader (-> proc .getErrorStream InputStreamReader. BufferedReader.)
+        alive? #(.isAlive proc)
+        exit-value #(try
+                      (.exitValue proc)
+                      (catch Exception _ nil))
+        write #(do (.write stdin-writer (str % "\n"))
+                   (.flush stdin-writer))
+        read #(.readLine stdout-reader)]
+    {:alive? alive?
+     :exit-value exit-value
+     :write write
+     :read read
+     :stdin   stdin-writer
+     :stdout  stdout-reader
+     :stderr  stderr-reader}))
--- test/forkbridge/core_test.clj
+++ test/forkbridge/core_test.clj
@@ -1,7 +1,29 @@
 (ns forkbridge.core-test
-  (:require [clojure.test :refer :all]
-            [forkbridge.core :refer :all]))
+  (:require [clojure.test :refer [deftest is]]
+            [forkbridge.core :refer [start-process]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest alive?-and-exit-value
+  (let [p (start-process ["clojure" "-M" "-e" "(do (Thread/sleep 1000) (System/exit 10))"])
+        alive? (:alive? p)
+        exit-value (:exit-value p)]
+    (is (true? (alive?)))
+    (is (nil? (exit-value)))
+    (Thread/sleep 1500)
+    (is (false? (alive?)))
+    (is (= 10 (exit-value)))))
+
+(deftest read-and-write
+  (let [p (start-process ["clojure"])
+        exit-value (:exit-value p)
+        read (:read p)
+        write (:write p)
+        header (read)]
+    (is (= "Clojure 1.12.0" header))
+
+    (write "(+ 10 20)")
+    (let [output (read)]
+      (is (= "user=> 30" output)))
+
+    (write "(System/exit 15)")
+    (Thread/sleep 500)
+    (is (= 15 (exit-value)))))
