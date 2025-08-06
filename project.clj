beeb491b7d266edecb63b671a8afabc422749aae
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2025-08-06 17:10:26 -0300

    Update LICENCE and project.clj.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject forkbridge "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject forkbridge "0.1.0"
+  :description "A small, well-tested Clojure library for spawning and interacting with subprocesses"
+  :url "https://github.com/martinjungblut/forkbridge"
+  :license {:name "MIT Licence"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :repl-options {:init-ns forkbridge.core})
